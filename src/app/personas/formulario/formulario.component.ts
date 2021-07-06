df25da870fe29ef9ef723c23d3ce4457d42f4da2
ivancostilla <ivan20punto88@gmail.com>
2021-07-06 20:33:51 -0300

    navegacion con objeto router

--- src/app/personas/formulario/formulario.component.ts
+++ src/app/personas/formulario/formulario.component.ts
@@ -1,4 +1,5 @@
 import { Component, ElementRef, EventEmitter, Output, ViewChild } from '@angular/core';
+import { Router } from '@angular/router';
 import { LoggingService } from '../../LoggingService.service';
 import { Persona } from '../../persona.model';
 import { PersonasService } from '../../persona.service';
@@ -14,20 +15,23 @@ export class FormularioComponent  {
   apellidoInput:string;
 
   constructor(private loggingService:LoggingService,
-    private personaService:PersonasService){
+    private personaService:PersonasService,
+    private router:Router
+    ){
   //comunicacion entre componentes usando servicios:
       this.personaService.saludar.subscribe( (indice: number) => {
         alert(indice)
       })
     }
 
-  agregarPersona(){
+  onGuardarPersona(){
     let persona1 = new Persona(
       this.nombreInput,
       this.apellidoInput);
     //this.loggingService.enviaMensajeAConsola('Enviamos persona con nombre:' + persona1.nombre + ', apellido:' + persona1.apellido);
     //this.personaCreada.emit(persona1);
     this.personaService.agregarPersona(persona1);
+    this.router.navigate(['personas'])
   }
 
 }
--- src/app/personas/personas.component.ts
+++ src/app/personas/personas.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 import { LoggingService } from '../LoggingService.service';
 import { Persona } from '../persona.model';
 import { PersonasService } from '../persona.service';
@@ -13,10 +14,15 @@ export class PersonasComponent implements OnInit {
   personas: Persona[] = [];
 
   constructor(private loggingService:LoggingService,
-              private personasService: PersonasService){}
+              private personasService: PersonasService,
+              private router:Router
+  ){}
 
   ngOnInit(): void {
     this.personas = this.personasService.personas;
+  }
+  agregar(){
+    this.router.navigate(['personas/agregar'])
   }
 
 }
